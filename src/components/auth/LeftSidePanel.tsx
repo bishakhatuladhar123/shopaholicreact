ad6c940442278562d0a597d6984e8968d2516ac2
Bishakha Tuladhar <bishakhatuladhar@gmail.com>
2026-03-23 12:55:17 +0545

    props

--- src/components/auth/LeftSidePanel.tsx
+++ src/components/auth/LeftSidePanel.tsx
@@ -1,10 +1,11 @@
 
 import Logo from "../logo/Logo";
+import {H1} from "../ui/typography/PageTitle";
 export default function LeftSidePanel() {
     return (
         <div className="w-1/3 bg-purple-300 rounded-md p-10 items-center justify-center flex flex-col gap-5 text-white">
            <Logo/>
-            <h1 className="text-6xl font-semibold text-red-500">Login Page</h1>
+            <H1 className="!text-green-100">Login Page</H1>
             <p className="text-center p-5">
                 Your shop’s best ally.
             </p>
--- src/components/auth/RightSidePanel.tsx
+++ src/components/auth/RightSidePanel.tsx
@@ -1,38 +1,43 @@
-export default function RightSidePanel()
-{
-    return(
+import { H1 } from "../ui/typography/PageTitle";
+export default function RightSidePanel() {
+    return (
         <div className="w-2/3 gap-10 flex flex-col bg-gray-100 p-10 rounded-md">
-        <div className="flex border-b border-b-green-900/30 pb-5">
-            <h1 className="text-6xl font-semibold  text-green-900">Login Form</h1>
-        </div>
-        <form action="" className="flex flex-col gap-5">
-            <div className="flex w-full items-center">
-                <label htmlFor="username" className="w-2/3 font-semibold " >User Name:{""}</label>
-                <div className="w-2/3 flex flex-col">
-                    <input type="email" className="border border-gray-300 w-full  p-2  rounded-md shadow bg-white" />
-                    <span className="text-red-800 tex-sm italic"></span>
+            <div className="flex border-b border-b-green-900/30 pb-5">
+                <H1 className="!text-green-900">
+                    <em>
+                        Login Form
+                    </em>
+                </H1>
+                {/* <h1 className="text-6xl font-semibold  text-green-900">Login Form</h1> */}
+            </div>
+            <form action="" className="flex flex-col gap-5">
+                <div className="flex w-full items-center">
+                    <label htmlFor="username" className="w-2/3 font-semibold " >User Name:{""}</label>
+                    <div className="w-2/3 flex flex-col">
+                        <input type="email" className="border border-gray-300 w-full  p-2  rounded-md shadow bg-white" />
+                        <span className="text-red-800 tex-sm italic"></span>
+                    </div>
                 </div>
-            </div>
-            <div className="flex w-full items-center">
-                <label htmlFor="password" className="w-2/3 font-semibold " >Password:</label>
-                <div className="w-2/3 flex flex-col">
-                    <input type="password" className="border border-gray-300 w-full  p-2  rounded-md shadow bg-white" />
-                    <span className="text-red-800 tex-sm italic"></span>
+                <div className="flex w-full items-center">
+                    <label htmlFor="password" className="w-2/3 font-semibold " >Password:</label>
+                    <div className="w-2/3 flex flex-col">
+                        <input type="password" className="border border-gray-300 w-full  p-2  rounded-md shadow bg-white" />
+                        <span className="text-red-800 tex-sm italic"></span>
+                    </div>
                 </div>
-            </div>
-            <div className="flex w-full items-center justify-end">
-                <a href="/forget-password" className="text-teal-500 italic text-sm hover:underline hover:text-teal-600 transition hover:scale-96">Forget Password?</a>
-            </div>
-            <div className="flext w-full items-center gap-3">
-                <button className="rounded-md cursor-pointer transition hover:scale-98 hover:bg-red-700 w-full bg-red-800 text-white flex items-center justify-center p-2">
-                    Reset
-                </button>
-                <button className="rounded-md cursor-pointer transition hover:scale-98 hover:bg-teal-700 w-full bg-teal-800 text-white flex items-center justify-center p-2">
-                    Submit
-                </button>
+                <div className="flex w-full items-center justify-end">
+                    <a href="/forget-password" className="text-teal-500 italic text-sm hover:underline hover:text-teal-600 transition hover:scale-96">Forget Password?</a>
+                </div>
+                <div className="flext w-full items-center gap-3">
+                    <button className="rounded-md cursor-pointer transition hover:scale-98 hover:bg-red-700 w-full bg-red-800 text-white flex items-center justify-center p-2">
+                        Reset
+                    </button>
+                    <button className="rounded-md cursor-pointer transition hover:scale-98 hover:bg-teal-700 w-full bg-teal-800 text-white flex items-center justify-center p-2">
+                        Submit
+                    </button>
 
-            </div>
-        </form>
-    </div>
+                </div>
+            </form>
+        </div>
     );
 }
